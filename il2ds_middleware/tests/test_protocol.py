1e96aab7df5073eb42e8e073ff57fe03945bb56f
Alexander Oblovatniy <oblovatniy@gmail.com>
2013-08-23 15:34:09 +0300

    irrelevant test case commented

--- il2ds_middleware/tests/test_protocol.py
+++ il2ds_middleware/tests/test_protocol.py
@@ -9,17 +9,17 @@ from il2ds_middleware.ds_emulator.service import RootService as DSService
 from il2ds_middleware.ds_emulator.protocol import ConsoleFactory as DSFactory
 
 
-class TestClientFactory(TestCase):
+# class TestClientFactory(TestCase):
 
-    def setUp(self):
-        self.dsfactory = DSFactory()
-        self.dsservice = DSService(self.dsfactory)
-        self.dsport = reactor.listenTCP(0, self.dsfactory)
+#     def setUp(self):
+#         self.dsfactory = DSFactory()
+#         self.dsservice = DSService(self.dsfactory)
+#         self.dsport = reactor.listenTCP(0, self.dsfactory)
 
-        self.addCleanup(self.dsport.stopListening)
-        self.addCleanup(self.dsservice.stopService)
+#         self.addCleanup(self.dsport.stopListening)
+#         self.addCleanup(self.dsservice.stopService)
 
-        self.dsservice.startService()
+#         self.dsservice.startService()
 
-    def test_connect_with_wrong_address(self):
-        pass
+#     def test_connect_with_wrong_address(self):
+#         pass
