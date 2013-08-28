5fbf452f13a9bc5d8b66e6e2e10ba37d9f9f0403
Alexander Oblovatniy <oblovatniy@gmail.com>
2013-08-28 22:18:22 +0300

    Test for failing connection added. #1

--- il2ds_middleware/tests/base.py
+++ il2ds_middleware/tests/base.py
@@ -12,6 +12,11 @@ from il2ds_middleware.ds_emulator.protocol import (ConsoleServerFactory,
 
 class BaseTestCase(TestCase):
 
+    console_server_host = "127.0.0.1"
+    console_server_port = 0
+    device_link_server_host = "127.0.0.1"
+    device_link_server_port = 0
+
     def setUp(self):
         self._listen_server()
         return self._connect_client()
@@ -26,42 +31,63 @@ class BaseTestCase(TestCase):
             'dl').got_requests
 
         from twisted.internet import reactor
-        self.console_server_port = reactor.listenTCP(
-            0, self.console_server_factory, interface="127.0.0.1")
-        self.dl_server_port = reactor.listenUDP(
-            0, self.dl_server, interface="127.0.0.1")
+        # Start console server
+        self.console_server_listener = reactor.listenTCP(
+            self.console_server_port, self.console_server_factory,
+            interface=self.console_server_host)
+        # Start Device Link server
+        self.dl_server_listener = reactor.listenUDP(
+            self.device_link_server_port, self.dl_server,
+            interface=self.device_link_server_host)
         self.service.startService()
 
     def _connect_client(self):
         self.console_client_factory = ConsoleClientFactory()
 
-    #     ds_host = self.dl_server_port.getHost()
-    #     self.dl_client = DeviceLinkClientProtocol((ds_host.host, ds_host.port))
+        # self.dl_client = DeviceLinkClientProtocol(
+        #     self.device_link_host_for_client)
 
         from twisted.internet import reactor
-        self.console_client_port = reactor.connectTCP(
-            "127.0.0.1", self.console_server_port.getHost().port,
-            self.console_client_factory)
-    #     self.dl_client_port = reactor.listenUDP(0, self.dl_client)
+        host, port = self.console_client_host_for_client
+        self.console_client_connector = reactor.connectTCP(
+            host, port, self.console_client_factory)
+        # self.dl_client_port = reactor.listenUDP(0, self.dl_client)
         return self.console_client_factory.on_connecting
 
     def tearDown(self):
         self.console_server_factory.on_data = None
         self.dl_server.on_request = None
         self.console_client_factory.receiver = None
-    #     self.dl_client.receiver = None
+        # self.dl_client.receiver = None
 
         console_server_stopped = defer.maybeDeferred(
-            self.console_server_port.stopListening)
+            self.console_server_listener.stopListening)
         dl_server_stopped = defer.maybeDeferred(
-            self.dl_server_port.stopListening)
-    #     dl_client_stopped = defer.maybeDeferred(
-    #         self.dl_client_port.stopListening)
-        self.console_client_port.disconnect()
+            self.dl_server_listener.stopListening)
+        # dl_client_stopped = defer.maybeDeferred(
+        #     self.dl_client_port.stopListening)
 
-        return defer.gatherResults([
+        dlist = [
             console_server_stopped, dl_server_stopped,
             # dl_client_stopped,
-            self.console_client_factory.on_connection_lost,
-            self.console_server_factory.on_connection_lost,
-            self.service.stopService(), ])
+            self.service.stopService(), ]
+        if self.console_client_connector is not None:
+            dlist.extent([
+                self.console_client_factory.on_connection_lost,
+                self.console_server_factory.on_connection_lost, ])
+            self.console_client_connector.disconnect()
+        return defer.gatherResults(dlist)
+
+    @property
+    def console_client_host_for_client(self):
+        if self.console_server_listener is None:
+            return (None, None)
+        endpoint = self.console_server_listener.getHost()
+        return endpoint.host, endpoint.port
+
+    @property
+    def device_link_host_for_client(self):
+        if self.dl_server_listener is None:
+            return (None, None)
+        endpoint = self.dl_server_listener.getHost()
+        return endpoint.host, endpoint.port
--- il2ds_middleware/tests/test_protocol.py
+++ il2ds_middleware/tests/test_protocol.py
@@ -1,11 +1,32 @@
 # -*- coding: utf-8 -*-
 
 from twisted.internet import defer
+from twisted.internet import error
 
 from il2ds_middleware.tests.base import BaseTestCase
 
 
-class TestConsoleClientFactoryWrongAddress(BaseTestCase):
+class ConsoleClientFactoryConnectionFailTestCase(BaseTestCase):
+
+    console_server_port = 20000
+    device_link_server_port = 10000
+
+    def setUp(self):
+
+        def on_connection_fail(err):
+            if isinstance(err.value, error.ConnectionRefusedError):
+                self.console_client_connector = None
+
+        d = super(TestConsoleClientFactoryWrongAddress, self).setUp()
+        return d.addErrback(on_connection_fail)
 
     def test_connection_fail(self):
-        pass
+        self.assertNot(self.console_client_connector)
+
+    @property
+    def console_client_host_for_client(self):
+        return self.console_server_host, self.console_server_port+1
+
+    @property
+    def device_link_host_for_client(self):
+        return self.device_link_server_host, self.device_link_server_port+1
