4edf261f4b281f7b556bed549692bc65c8c42a76
Alexander Oblovatniy <oblovatniy@gmail.com>
2013-08-31 16:43:01 +0300

    Log watching service base class extracted. #5

--- il2ds_middleware/ds_emulator/tests/service.py
+++ il2ds_middleware/ds_emulator/tests/service.py
@@ -2,26 +2,23 @@
 
 import datetime
 
-from twisted.application.internet import TimerService
-from twisted.internet import defer
+from twisted.python import log
+
+from il2ds_middleware.service import LogWatchingBaseService
 
 
-class LogWatchingService(TimerService):
+class LogWatchingService(LogWatchingBaseService):
 
     receiver = None
-    log_file = None
 
-    def __init__(self, log_path, interval):
-        self.log_path = log_path
-        TimerService.__init__(self, interval, self.do_watch)
-
-    def do_watch(self):
-        self.log_file.seek(self.log_file.tell())
-        if self.receiver is not None:
-            for line in self.log_file.readlines():
-                time, data = self._parse_line(line)
-                if data is not None:
-                    self.receiver(data)
+    def got_line(self, line):
+        try:
+            time, data = self._parse_line(line)
+        except ValueError:
+            log.err("Failed to parse line:\n\t{0}".format(line))
+        else:
+            if self.receiver is not None:
+                self.receiver(data)
 
     def _parse_line(self, line):
         idx = line.find(']')
@@ -29,26 +26,9 @@ class LogWatchingService(TimerService):
         format = "%I:%M:%S %p"
         try:
             time = datetime.datetime.strptime(time_raw, format)
-        except:
+        except ValueError:
             format = "%b %d, %Y " + format
-            try:
-                time = datetime.datetime.strptime(time_raw, format)
-            except:
-                return (None, None)
+            time = datetime.datetime.strptime(time_raw, format)
         finally:
             data = line[idx + 1:].lstrip()
             return time, data
-
-    def startService(self):
-        if self.log_path is None:
-            return
-        self.log_file = open(self.log_path, 'r')
-        TimerService.startService(self)
-
-    def stopService(self):
-        if self.log_file is None:
-            return defer.succeed(None)
-        else:
-            self.log_file.close()
-            self.log_file = None
-            return TimerService.stopService(self)
---
+++ il2ds_middleware/service.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+from twisted.application.internet import TimerService
+from twisted.internet import defer
+
+
+class LogWatchingBaseService(TimerService):
+
+    def __init__(self, log_path, interval=0.1):
+        self.log_file = None
+        self.log_path = log_path
+        TimerService.__init__(self, interval, self.do_watch)
+
+    def do_watch(self):
+        self.log_file.seek(self.log_file.tell())
+        for line in self.log_file.readlines():
+            self.got_line(line)
+
+    def got_line(self, line):
+        raise NotImplementedError
+
+    def startService(self):
+        if self.log_file is not None:
+            return
+        self.log_file = open(self.log_path, 'r')
+        TimerService.startService(self)
+
+    def stopService(self):
+        if self.log_file is None:
+            return defer.succeed(None)
+        else:
+            self.log_file.close()
+            self.log_file = None
+            return TimerService.stopService(self)
+
+
+class LogWatchingService(LogWatchingBaseService):
+
+    def got_line(self, line):
+        pass
